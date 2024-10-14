36ce34b5ccd3233517c9124b17c8cd5050a0041f
MohamedFathi742 <mofathi742002@gmail.com>
2024-10-14 09:18:07 +0300

    Add project files.

---
+++ areaOfCirculi/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace areaOfCirculi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //area of circuli
+            Console.WriteLine("wellcome to area of circuli app ");
+
+            // ask user to enter raduis
+            Console.WriteLine("please enter raduis ");
+            //read raduis 
+            double dRaduis = 0;
+            bool isConverted = double.TryParse(Console.ReadLine(), out dRaduis);
+            // calc area
+            const double pi = 3.14;
+            double calcArea = dRaduis * dRaduis * pi;
+            // print area
+            Console.WriteLine($"area of circuli:{calcArea}");
+            Console.WriteLine("\n");
+        }
+    }
+}
